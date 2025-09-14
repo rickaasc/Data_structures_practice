d157cec309f099e63527affe38decbf053973c2f
rick <18531284286@163.com>
2025-09-14 17:12:58 +0800

    init commit

---
+++ Tree/2branchTree.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+//二叉树节点定义
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
